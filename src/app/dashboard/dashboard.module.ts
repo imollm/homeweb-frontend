8aa78f152f7b640f80db860ae98623ffbc033363
Ivan Moll <ivanmoll07@gmail.com>
2021-03-23 02:52:09 +0100

    Add footer and sidebar of dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, HeaderComponent],
+  declarations: [DashboardComponent, HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
