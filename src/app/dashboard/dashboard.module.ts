fc6da228aee939cf53ee37c0d8ccca4aa212fc13
Ivan Moll <ivanmoll07@gmail.com>
2021-03-23 01:03:47 +0100

    Add header of dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, HeaderComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
