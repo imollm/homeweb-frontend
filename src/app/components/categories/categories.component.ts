925701f21bd3046fb20661c11954bbbeabf23e37
Ivan <ivanmoll07@gmail.com>
2021-03-26 19:49:02 +0100

    Work with category component

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoriesService} from '../../services/_category/categories.service';
+import {ICategory} from '../../models/category';
+import {AlertService} from '../../_alert/alert.service';
+import {EndPointMapper} from '../../api/end-point-mapper';
+import {ImageService} from '../../services/_image/image.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  title = 'CATEGORIES';
+  categories: ICategory[] = [];
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private alertService: AlertService,
+    private endPointMapper: EndPointMapper,
+    private imageService: ImageService
+  ) { }
 
   ngOnInit(): void {
+    this.getCategories().then(c => { console.log(this.categories); });
   }
 
+  private async getCategories(): Promise<any> {
+    const promise = await this.categoriesService.getAllCategories();
+
+    if (promise.success) {
+      this.categories = promise.data;
+    } else {
+      this.alertService.error(promise.message);
+    }
+  }
+
+  async getImage(id: string): Promise<Blob> {
+    return await this.imageService.getImage('categories', id);
+  }
 }
