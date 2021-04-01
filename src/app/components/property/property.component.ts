38dae86123f7a55845e1d8be7dada5de5b1a7a0c
Ivan Moll <ivanmoll07@gmail.com>
2021-04-01 03:30:32 +0200

    Individual property page

---
+++ src/app/components/property/property.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {MessageService} from '../../services/message.service';
+import {Property} from '../../models/property';
+import {HelpersService} from "../../services/_helpers/helpers.service";
+
+@Component({
+  selector: 'app-property',
+  templateUrl: './property.component.html',
+  styleUrls: ['./property.component.css']
+})
+export class PropertyComponent implements OnInit {
+
+  property: Property;
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.messageService.currentMessage.subscribe((msg) => {
+      this.property = msg;
+      this.property.energetic_certificate = HelpersService.EnergeticCertificate(this.property.energetic_certificate);
+      console.log(this.property);
+    });
+  }
+
+}
