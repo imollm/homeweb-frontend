80bf5e4a82eb9d05b841d1b7b714678fb9406b8c
Ivan Moll <ivanmoll07@gmail.com>
2021-04-03 23:20:20 +0200

    Create contact page and contact-form component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { faUserAlt, faEnvelope, faAt, faPenSquare } from '@fortawesome/free-solid-svg-icons';
-import {IContact} from '../../models/contact';
-import {ContactService} from '../../services/_contact/contact.service';
-import {AlertService} from '../../_alert/alert.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -12,65 +7,9 @@ import {AlertService} from '../../_alert/alert.service';
 })
 export class ContactComponent implements OnInit {
 
-  @Input() propertyRef: string;
-
-  title = 'Contacte';
-  contactForm: FormGroup;
-  // @ts-ignore
-  contact: IContact = {};
-
-  faUserName = faUserAlt;
-  faEmail = faAt;
-  faSubject = faPenSquare;
-  faMessage = faEnvelope;
-
-  constructor(private fb: FormBuilder, private contactService: ContactService, private alertService: AlertService) {
-    this.contactForm = this.fb.group({
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      subject: new FormControl('', [Validators.required]),
-      message: new FormControl('', [Validators.required])
-    });
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    if (this.propertyRef !== undefined) {
-      this.contactForm.get('subject').setValue('Me interessa, ref: ' + this.propertyRef);
-    }
   }
-
-  onSubmit(): void {
-    if (this.contactForm.valid) {
-      this.setValues();
-      this.contactService.sendEmail(this.contact).subscribe((res) => {
-        console.log(res);
-        if (res.success) {
-          this.alertService.success(res.message);
-        } else{
-          this.alertService.error(res.message);
-        }
-      });
-      this.clearForm();
-    }
-    console.log(this.contactForm.get('name').valid);
-  }
-
-  private setValues(): void {
-    Object.keys(this.contactForm.controls).forEach((key) => {
-      this.contact[key] = this.contactForm.get(key).value;
-    });
-  }
-
-  private clearForm(): void {
-    this.contactForm.reset();
-  }
-
-  get name(): AbstractControl { return this.contactForm.get('name'); }
-
-  get email(): AbstractControl { return this.contactForm.get('email'); }
-
-  get subject(): AbstractControl { return this.contactForm.get('subject'); }
-
-  get message(): AbstractControl { return this.contactForm.get('message'); }
 
 }
