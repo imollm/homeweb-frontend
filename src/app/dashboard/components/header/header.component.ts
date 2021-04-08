ba0a01e2aff607fbf496e515076389ed65e4e698
Ivan <ivanmoll07@gmail.com>
2021-04-08 17:10:54 +0200

    Add bars icon

--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  faBars = faBars;
 
   constructor() { }
 
