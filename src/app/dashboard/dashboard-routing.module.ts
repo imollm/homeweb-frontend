12de1c552c510afd93b6c066c19d5762d6d73930
Ivan <ivanmoll07@gmail.com>
2021-04-08 17:03:47 +0200

    Layout not found component, and establish home and ** route on dashboard routing module

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { DashboardComponent } from './dashboard.component';
+import {DashboardComponent} from './dashboard.component';
+import {HomeComponent} from './components/home/home.component';
+import {NotFoundComponent} from '../components/not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: '**', component: NotFoundComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {
+}
