f4ce403db5bfafb1392a96972457f8f3370f2d7c
Ivan <ivanmoll07@gmail.com>
2021-04-12 21:02:19 +0200

    Added other child components roles for dashboard-home

---
+++ src/app/dashboard/components/home/employee/employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-home-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
