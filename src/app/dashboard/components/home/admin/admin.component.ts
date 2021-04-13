d0aeaa128f602654df24acafda6df57006f8fd16
Ivan <ivanmoll07@gmail.com>
2021-04-13 20:40:03 +0200

    Add chart dependency and construct sales chart on admin dashboard home

--- src/app/dashboard/components/home/admin/admin.component.ts
+++ src/app/dashboard/components/home/admin/admin.component.ts
@@ -4,6 +4,7 @@ import {HelpersService} from '../../../../services/_helpers/helpers.service';
 import {SalesService} from '../../../../services/_sale/sales.service';
 import {IDashboardTable} from '../../../../models/dashboard-table';
 import {PropertiesService} from '../../../../services/_property/properties.service';
+import {Color, Label, SingleDataSet} from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard-home-admin',
@@ -12,12 +13,20 @@ import {PropertiesService} from '../../../../services/_property/properties.servi
 })
 export class AdminComponent implements OnInit {
 
+  title = 'Admin Dashboard';
+  actualYear: number;
+
   totalProperties: IDashboardCard = {} as IDashboardCard;
   totalSales: IDashboardCard = {} as IDashboardCard;
   totalAmountOfSales: IDashboardCard = {} as IDashboardCard;
   totalSalesLastMonth: IDashboardCard = {} as IDashboardCard;
 
   dataTable: IDashboardTable = {} as IDashboardTable;
+
+  chartData: SingleDataSet[] = [];
+  chartLabels: Label[] = [];
+  chartColors: Color[] = [];
+  chartLegend = false;
 
   constructor(
     private salesService: SalesService,
@@ -26,6 +35,7 @@ export class AdminComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHome();
+    this.actualYear = (new Date()).getFullYear();
   }
 
   private getHome(): void {
@@ -47,7 +57,8 @@ export class AdminComponent implements OnInit {
           {colName: 'reference', text: 'Referència'},
           {colName: 'title', text: 'Títol'},
           {colName: 'price', text: 'Preu'},
-          {colName: 'built_meters', text: 'Metres edificats'}
+          {colName: 'built_meters', text: 'Metres edificats'},
+          {colName: 'created_at', text: 'Creat dia'}
         ];
       }
     });
@@ -57,5 +68,29 @@ export class AdminComponent implements OnInit {
         this.totalProperties.value      = response.data.length;
       }
     });
+    this.salesService.getLastYear().then((response) => {
+      if (response.success) {
+        const sales = response.data;
+        sales.map((sale) => {
+          this.chartLabels.push(sale.month);
+          this.chartData.push(sale.amount);
+        });
+      }
+    }).then(() => {
+      this.setSalesChartConfig();
+    });
+  }
+
+  private setSalesChartConfig(): void {
+    this.chartColors = [
+      { // green
+        backgroundColor: 'rgb(126,219,53)',
+        borderColor: 'green',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      }
+    ];
   }
 }
