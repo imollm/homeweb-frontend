909a1027bf9336161732b61e29fd2c96e29ec828
Ivan <ivanmoll07@gmail.com>
2021-04-15 00:42:11 +0200

    Add users service and energetic certificate enum.

---
+++ src/app/services/_user/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {EndPointMapper} from '../../api/end-point-mapper';
+import {ApiResponseI} from '../../models/api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  resource = 'users';
+
+  constructor(
+    private httpClient: HttpClient,
+    private endPointMapper: EndPointMapper
+  ) { }
+
+  getOwners(): Promise<ApiResponseI> {
+    const endpoint = this.endPointMapper.getEndPointUrl(this.resource, 'owners');
+    return this.httpClient.get<ApiResponseI>(endpoint).toPromise();
+  }
+}
