fc2494ae03d15d52f8a2b1285009a0ff2fd90d7f
Ivan <ivanmoll07@gmail.com>
2021-04-23 01:15:16 +0200

    Add new fields on IMaps, location is for position of maps center, markers is array of locations, if we need to print some locations of properties on map, and getLocation say if we need to get the location when new property is created.

--- src/app/models/maps.ts
+++ src/app/models/maps.ts
@@ -1,6 +1,11 @@
-export interface IMaps {
+export interface ILocation {
   lat: number;
   lng: number;
+}
+export interface IMaps {
+  location: ILocation;
+  markers?: ILocation[];
   zoom: number;
   mapType: string;
+  getLocation: boolean;
 }
