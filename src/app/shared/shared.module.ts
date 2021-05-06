f09f0e6801281dc47acd3a70f15988cd5f5fa710
Ivan <ivanmoll07@gmail.com>
2021-05-06 19:21:03 +0200

    Share TableComponent and others, with public module and dashboard module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapsComponent } from '../components/maps/maps.component';
+import { TableComponent } from '../dashboard/components/table/table.component';
 import { AgmCoreModule } from '@agm/core';
+import {RouterModule} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [MapsComponent],
+  declarations: [MapsComponent, TableComponent],
   imports: [
     CommonModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCNwaQTFTIunXT6r9Ugdk5GowFReVaMdAY'
     }),
+    RouterModule,
+    FontAwesomeModule,
   ],
-  exports: [MapsComponent]
+  exports: [MapsComponent, TableComponent]
 })
 export class SharedModule { }
