b41e5011db52d2e92b4122c946ed9ca891aee200
Ivan <ivanmoll07@gmail.com>
2021-05-14 18:42:34 +0200

    Add delete modal on table component to show alert confirmation before delete resource.

--- src/app/dashboard/components/table/table.component.ts
+++ src/app/dashboard/components/table/table.component.ts
@@ -1,8 +1,10 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {IDashboardTable} from '../../../models/dashboard-table';
 import {HelpersService} from '../../../services/_helpers/helpers.service';
 import {faTimes, faEye, faPencilAlt} from '@fortawesome/free-solid-svg-icons';
 import {IActionButtons} from '../../../models/action-buttons';
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-table',
@@ -19,9 +21,27 @@ export class TableComponent implements OnInit, OnChanges {
   faEdit = faPencilAlt;
   faDelete = faTimes;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  showDeleteModal(url: string): void {
+    Swal.fire({
+      title: 'Estas segur de voler eliminar?',
+      text: 'L\'acció no es podra desfer',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#589c1d',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, elimina!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate([url]);
+      }
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
